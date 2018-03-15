fe1a6f005e21d1b11e4f03cd78e8d567215bf714
TheBestLogic <my.the.best.logic@gmail.com>
2018-03-15 16:10:51 +0200

    Почти сделал функции CreateIN/CreateOUT. Осталось в них сами процедуры считывания- записи добавить. Нужно еще зделать потоки.

--- src/connect.cpp
+++ src/connect.cpp
@@ -12,44 +12,66 @@ void DoConnect::CreateIN()
 		static resmgr_connect_funcs_t		 connect_funcs_IN;
 		static resmgr_io_funcs_t 		io_funcs_IN;
 		static iofunc_attr_t attr_IN;
+		throw "\"\" - failed";
 		 */
 		// инициализация интерфейса диспетчеризации
 	 	if ((dpp_IN = dispatch_create()) == NULL)
 	 	{
-	 		perror("allocate dispatch"), exit(EXIT_FAILURE);
+	 		throw "\"dpp_IN = dispatch_create()\" - failed";
 	 	}
-
 	 	// инициализация атрибутов менеджера
-	 	memset(&resmgr_attr, 0, sizeof resmgr_attr);
-	 	resmgr_attr.nparts_max = 1;
-	 	resmgr_attr.msg_max_size = 2048;
+	 	memset(&resmgr_attr_IN, 0, sizeof resmgr_attr_IN);
+	 	resmgr_attr_IN.nparts_max = 1;
+	 	resmgr_attr_IN.msg_max_size = 2048;
 	 	// инициализация таблиц функций обработчиков
-	 	static resmgr_connect_funcs_t		 connect_funcs;
-	 	static resmgr_io_funcs_t 		io_funcs;
-	 	iofunc_func_init(_RESMGR_CONNECT_NFUNCS, &connect_funcs,
-	  	_RESMGR_IO_NFUNCS, &io_funcs);
+	 	iofunc_func_init(_RESMGR_CONNECT_NFUNCS, &connect_funcs_IN,_RESMGR_IO_NFUNCS, &io_funcs_IN);
 	 	// здесь нами дописан всего один обработчик - операции read,
 	 	// все остальное делается менеджером по умолчанию!
-	 	io_funcs.read = prior_read;
+
+	 	io_funcs_IN.read = prior_read;//must be my read function
+
 	 	// инициализация атрибутной структуры, используемой
 	 	// устройством.
-	 	static iofunc_attr_t attr;
-	 	iofunc_attr_init(&attr, S_IFNAM | 0666, 0, 0);
+	 	iofunc_attr_init(&attr_IN, S_IFNAM | 0666, 0, 0);
 	 	// здесь создается путевое имя для менеджера
-	 	id = resmgr_attach(dpp, &resmgr_attr, "/dev/prior", _FTYPE_ANY, 0, &connect_funcs, &io_funcs, &attr);
+	 	id = resmgr_attach(dpp_IN, &resmgr_attr_IN, "/dev/prior_IN", _FTYPE_ANY, 0, &connect_funcs_IN, &io_funcs_IN, &attr_IN);
 		if (id == -1)
-	 		perror("attach name"), exit(EXIT_FAILURE);
-	 	ctp = dispatch_context_alloc(dpp);
+	 		throw "\"id = resmgr_attach(dpp_IN, &resmgr_attr_IN, \"/dev/prior_IN\", _FTYPE_ANY, 0, &connect_funcs_IN, &io_funcs_IN, &attr_IN)\" - failed";
+	 	ctp_IN = dispatch_context_alloc(dpp_IN);
 }
 
 void DoConnect::CreateOUT()
 {
+			// инициализация интерфейса диспетчеризации
+		 	if ((dpp_OUT = dispatch_create()) == NULL)
+		 	{
+		 		throw "\"dpp_OUT = dispatch_create()\" - failed";
+		 	}
+		 	// инициализация атрибутов менеджера
+		 	memset(&resmgr_attr_OUT, 0, sizeof resmgr_attr_OUT);
+		 	resmgr_attr_OUT.nparts_max = 1;
+		 	resmgr_attr_OUT.msg_max_size = 2048;
+		 	// инициализация таблиц функций обработчиков
+		 	iofunc_func_init(_RESMGR_CONNECT_NFUNCS, &connect_funcs_OUT,_RESMGR_IO_NFUNCS, &io_funcs_OUT);
+		 	// здесь нами дописан всего один обработчик - операции read,
+		 	// все остальное делается менеджером по умолчанию!
 
+		 	io_funcs_OUT.write =  prior_write;//must be my write function
+
+		 	// инициализация атрибутной структуры, используемой
+		 	// устройством.
+		 	iofunc_attr_init(&attr_OUT, S_IFNAM | 0666, 0, 0);
+		 	// здесь создается путевое имя для менеджера
+		 	id = resmgr_attach(dpp_OUT, &resmgr_attr_OUT, "/dev/prior_OUT", _FTYPE_ANY, 0, &connect_funcs_OUT, &io_funcs_OUT, &attr_OUT);
+			if (id == -1)
+		 		throw "\"id = resmgr_attach(dpp_OUT, &resmgr_attr_OUT, \"/dev/prior_OUT\", _FTYPE_ANY, 0, &connect_funcs_OUT, &io_funcs_OUT, &attr_OUT)\" - failed";
+		 	ctp_OUT = dispatch_context_alloc(dpp_OUT);
 }
 
 void DoConnect::DoConnect()
 {
-
+	DoConnect::CreateIN();
+	DoConnect::CreateOUT();
 }
 
 void DoConnect::~DoConnect()
