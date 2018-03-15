4fff5ff628c3ac110437894aa473036a802eb1e6
TheBestLogic <my.the.best.logic@gmail.com>
2018-03-15 15:42:42 +0200

    Начал пилить коннект

--- src/connect.cpp
+++ src/connect.cpp
@@ -1,17 +1,58 @@
 #include "connect.h"
-#include "program.h"
 using namespace std;
 using namespace DataSend;
 
-void DoConnect::StartTimer( Data &D )
-	{
-		//create attrib if need
-		while( 1 )
-		{
-			/*pthread_create(NULL, &attr, new_thread, NULL);
-			pthread_create(NULL, &attr, new_thread, NULL);
-			int pthread_join(pthread_t thread, void **value_ptr);
-			int pthread_join(pthread_t thread, void **value_ptr);
-			sleep(20);*/
-		}
-	}
+void DoConnect::CreateIN()
+{
+		/*
+		 * resmgr_attr_t resmgr_attr_IN;
+		dispatch_t *dpp_IN;
+		dispatch_context_t *ctp_IN;
+		int id_IN;
+		static resmgr_connect_funcs_t		 connect_funcs_IN;
+		static resmgr_io_funcs_t 		io_funcs_IN;
+		static iofunc_attr_t attr_IN;
+		 */
+		// инициализация интерфейса диспетчеризации
+	 	if ((dpp_IN = dispatch_create()) == NULL)
+	 	{
+	 		perror("allocate dispatch"), exit(EXIT_FAILURE);
+	 	}
+
+	 	// инициализация атрибутов менеджера
+	 	memset(&resmgr_attr, 0, sizeof resmgr_attr);
+	 	resmgr_attr.nparts_max = 1;
+	 	resmgr_attr.msg_max_size = 2048;
+	 	// инициализация таблиц функций обработчиков
+	 	static resmgr_connect_funcs_t		 connect_funcs;
+	 	static resmgr_io_funcs_t 		io_funcs;
+	 	iofunc_func_init(_RESMGR_CONNECT_NFUNCS, &connect_funcs,
+	  	_RESMGR_IO_NFUNCS, &io_funcs);
+	 	// здесь нами дописан всего один обработчик - операции read,
+	 	// все остальное делается менеджером по умолчанию!
+	 	io_funcs.read = prior_read;
+	 	// инициализация атрибутной структуры, используемой
+	 	// устройством.
+	 	static iofunc_attr_t attr;
+	 	iofunc_attr_init(&attr, S_IFNAM | 0666, 0, 0);
+	 	// здесь создается путевое имя для менеджера
+	 	id = resmgr_attach(dpp, &resmgr_attr, "/dev/prior", _FTYPE_ANY, 0, &connect_funcs, &io_funcs, &attr);
+		if (id == -1)
+	 		perror("attach name"), exit(EXIT_FAILURE);
+	 	ctp = dispatch_context_alloc(dpp);
+}
+
+void DoConnect::CreateOUT()
+{
+
+}
+
+void DoConnect::DoConnect()
+{
+
+}
+
+void DoConnect::~DoConnect()
+{
+
+}
--- src/connect.h
+++ src/connect.h
@@ -1,16 +1,35 @@
 #pragma once
 #include "program.h"
+#include <sys/iofunc.h>
+#include <sys/dispatch.h>
 
 namespace DataSend{
 class Data;
 class DoConnect{
 
-	pthread_t ThreadsIn[3];
-	pthread_t ThreadsOut[3];
-	pthread_mutex_t Mutex;
-	//resmgr_connect_funcs_t	connect_funcs;
-	//resmgr_io_funcs_t io_funcs;
-//muter read write
+		pthread_t ThreadsIn[3];
+		pthread_t ThreadsOut[3];
+		pthread_mutex_t Mutex;
+		//IN
+		resmgr_attr_t resmgr_attr_IN;
+		dispatch_t *dpp_IN;
+		dispatch_context_t *ctp_IN;
+		int id_IN;
+		static resmgr_connect_funcs_t		 connect_funcs_IN;
+		static resmgr_io_funcs_t 		io_funcs_IN;
+		static iofunc_attr_t attr_IN;
+		//OUT
+		resmgr_attr_t resmgr_attr_OUT;
+		dispatch_t *dpp_OUT;
+		dispatch_context_t *ctp_OUT;
+		int id_OUT;
+		static resmgr_connect_funcs_t		 connect_funcs_OUT;
+		static resmgr_io_funcs_t 		io_funcs_OUT;
+		static iofunc_attr_t attr_OUT;
+		//muter read write
+		void CreateIN();
+		void CreateOUT();
+
 	public:
 		DoConnect();
 		~DoConnect();
@@ -21,7 +40,6 @@ class DoConnect{
  * connect to file write(mutex)
  */
 
- void StartTimer( Data &Data );
 };
 
 }
