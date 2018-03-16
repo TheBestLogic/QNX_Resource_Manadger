006ae15a74ca7dfbcb85ae3c1223ad43a2af338d
TheBestLogic <my.the.best.logic@gmail.com>
2018-03-16 16:33:18 +0200

    пытаюсь связать ресурсы с вводом выводом

--- src/mprogram.cpp
+++ src/mprogram.cpp
@@ -2,43 +2,69 @@
 using namespace std;
 extern pthread_t Threads[3];
 
+using namespace std;
 using namespace DataSend;
 
 	Data::Data()
 	{
-		pthread_mutex_init( &Mutex , NULL);
+		pthread_mutex_init( &MutexIN , NULL);
+		pthread_mutex_init( &MutexOUT , NULL);
 	}
 
 	Data::~Data()
 	{
-		while( !DataQueue.empty() )
+		while( !DataQueueIN.empty() )
 		{
-			DataQueue.pop();
+			DataQueueIN.pop();
 		}
-		pthread_mutex_destroy( &Mutex );
+		while( !DataQueueOUT.empty() )
+		{
+			DataQueueOUT.pop();
+		}
+		pthread_mutex_destroy( &MutexIN );
+		pthread_mutex_destroy( &MutexOUT );
 	}
 
-	void Data::GetData( int In )
+	void Data::GetData( std::string In )
 	{
-		pthread_mutex_lock( &Mutex );
-		DataQueue.push( In );
-		pthread_mutex_unlock( &Mutex );
+		pthread_mutex_lock( &MutexIN );
+		DataQueueIN.push( In );
+		pthread_mutex_unlock( &MutexIN );
 	}
 
-
-	int Data::SendData()
+	void Data::QueueToResourse()
 	{
-		pthread_mutex_lock( &Mutex );
-		int buf = DataQueue.front();
-		DataQueue.pop();
-		pthread_mutex_unlock( &Mutex );
+		string Buffer = {0};
+		Buffer += DataQueue.front() + '\n';
+		DataQueueOUT.pop();
+		pthread_mutex_lock( &MutexIN );
+		pthread_mutex_lock( &MutexOUT );
+		PtText
+		pthread_mutex_unlock( &MutexOUT );
+		pthread_mutex_unlock( &MutexIN );
+		memset(Buffer, 0, Buffer.size());
+	}
+
+	void Data::ResourseToQueue()
+	{
+		char *ch;
+		PtGetResources(PtText,1 ,ch);
+		pthread_mutex_lock( &MutexIN );
+		pthread_mutex_lock( &MutexOUT );
+
+		pthread_mutex_unlock( &MutexOUT );
+		pthread_mutex_unlock( &MutexIN );
+	}
+
+	std::string Data::SendData()
+	{
+		pthread_mutex_lock( &MutexOUT );
+		std::string buf = DataQueue.front();
+		DataQueueOUT.pop();
+		pthread_mutex_unlock( &MutexOUT );
 		return buf;
 	}
 
-	void Data::ResourseToQueue(char *R)
-	{
-
-	}
 
 		
 	
--- src/program.h
+++ src/program.h
@@ -3,21 +3,26 @@
 #include <queue>
 #include <pthread.h>
 #include "connect.h"
+#include <Pt.h>
+#include <string>
 using namespace std;
 
 
 namespace DataSend{
 
 class Data{
-		pthread_mutex_t Mutex;
-		queue<int> DataQueue;
+		pthread_mutex_t MutexIN;
+		pthread_mutex_t MutexOUT;
+		queue<string> DataQueueIN;
+		queue<string> DataQueueOUT;
 	friend class DoConnect;
 	public:
 		Data();
 		~Data();
-		void GetData(int In);
-		int SendData();
+		void GetData(std::string In);
+		std::string SendData();
 		void ResourseToQueue(char *R);
+		void QueueToResourse(char *R);
 };
  	
 
